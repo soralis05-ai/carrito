3d7b97b4704b62dd2dd9b5a94a294cdf3ea48f95
Almapunt <contacto@almapunt.com>
2026-03-14 15:03:37 +0100

    Fix: Import session in app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
