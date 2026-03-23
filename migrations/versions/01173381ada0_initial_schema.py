ea1be3735d3e5f9154aecc1c0fd2c3123f769e76
Almapunt <contacto@almapunt.com>
2026-03-23 19:34:34 +0100

    Feat: Flask-Migrate inicial + migración inicial MySQL - v3.0.19

---
+++ migrations/versions/01173381ada0_initial_schema.py
@@ -0,0 +1,24 @@
+"""initial_schema
+
+Revision ID: 01173381ada0
+Revises: 
+Create Date: 2026-03-23 19:33:58.653124
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '01173381ada0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
